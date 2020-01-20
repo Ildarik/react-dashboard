9c6b27b6cecd2d1d7aafc056b314ed01555dfe98
Ildar Nogmanov <nogmanov@gmail.com>
2020-01-20 23:26:03 +0300

    Add button component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Button from "./components/Button";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container">
+      <header>
+        <img src="" className="logo" />
+        <Button type="primary">Добавить товар</Button>
+        <Button>Добавить категорию</Button>
+        <Button type="danger">Удалить</Button>
       </header>
     </div>
   );
---
+++ src/components/Button/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import styled from "styled-components";
+
+const handleType = type => {
+  switch (type) {
+    case "primary":
+      return "color: #fff; background-color: #1890ff;";
+    case "danger":
+      return "color: #fff; background-color: #ff4d4f;";
+    default:
+      return "color: rgba(0, 0, 0, 0.65); background-color: #fff;border: 1px solid rgb(217, 217, 217);";
+  }
+};
+const Button = styled.button`
+  padding: 8px 12px;
+  border: none;
+  border-radius: 4px;
+  ${({ type }) => handleType(type)};
+`;
+
+export default ({ type, children }) => {
+  return <Button type={type}>{children}</Button>;
+};
