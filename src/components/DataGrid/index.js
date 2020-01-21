9ebb9867bc819ff06bc1731a4a9f5fb7c0dbc159
Ildar Nogmanov <nogmanov@gmail.com>
2020-01-21 17:47:48 +0300

    Add mock data

--- src/components/DataGrid/index.js
+++ src/components/DataGrid/index.js
@@ -1,38 +1,8 @@
 import React from "react";
 import styled from "styled-components";
 import Button from "../Button";
-import Categories from "../Categories"
-
-const mock = [
-  {
-    id: "1",
-    name: "Товар 1",
-    category: "Категория 1",
-    price: "2200",
-    sell: "2500"
-  },
-  {
-    id: "2",
-    name: "Товар 2",
-    category: "Категория 2",
-    price: "2200",
-    sell: "2500"
-  },
-  {
-    id: "3",
-    name: "Товар 3",
-    category: "Категория 3",
-    price: "2200",
-    sell: "2500"
-  },
-  {
-    id: "4",
-    name: "Товар 1",
-    category: "Без категории",
-    price: "2200",
-    sell: "2500"
-  }
-];
+import Categories from "../Categories";
+import mock from "../../mock";
 
 const DataGrid = styled.div`
   display: flex;
---
+++ src/mock/index.js
@@ -0,0 +1,30 @@
+export default [
+  {
+    id: "1",
+    name: "Товар 1",
+    category: "Категория 1",
+    price: "2200",
+    sell: "2500"
+  },
+  {
+    id: "2",
+    name: "Товар 2",
+    category: "Категория 2",
+    price: "2200",
+    sell: "2500"
+  },
+  {
+    id: "3",
+    name: "Товар 3",
+    category: "Категория 3",
+    price: "2200",
+    sell: "2500"
+  },
+  {
+    id: "4",
+    name: "Товар 1",
+    category: "Без категории",
+    price: "2200",
+    sell: "2500"
+  }
+];
