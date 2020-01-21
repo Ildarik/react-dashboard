d07382394ea7a777ac43c16dc8a793abba48d410
Ildar Nogmanov <nogmanov@gmail.com>
2020-01-21 10:22:09 +0300

    Add logo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from "react";
 import "./App.css";
 import Button from "./components/Button";
+import styled from "styled-components";
+import logo from "./logo.svg";
+
+
+const Logo = styled.img`
+  height: 20vmin;
+`;
 
 function App() {
   return (
     <div className="container">
-      <header>
-        <img src="" className="logo" />
+        <Logo src={logo} />
         <Button type="primary">Добавить товар</Button>
         <Button>Добавить категорию</Button>
         <Button type="danger">Удалить</Button>
