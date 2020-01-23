ee1ba3a17f79b9e59d35145fb79a62a6780a1d5c
Ildar Nogmanov <nogmanov@gmail.com>
2020-01-23 12:11:34 +0300

    No need

--- src/App.js
+++ src/App.js
@@ -7,30 +7,24 @@ import DataGrid from "./components/DataGrid";
 import Modal from "./components/Modal";
 import styled from "styled-components";
 
-const App = styled.div`
-  ${({ showModal }) => showModal && "filter: blur(2px)"};
-`;
-
 export default () => {
   const [showModal, setShowModal] = useState(false);
 
   return (
-    <App>
-      <div className="container">
-        <Header>
-          <Logo />
-          <Button
-            onClick={() => setShowModal(!showModal)}
-            type="primary"
-            styles="margin-right: 10px; margin-left: 30px;"
-          >
-            Добавить товар
-          </Button>
-          <Button type="primary">Добавить категорию</Button>
-        </Header>
-        <DataGrid />
-        <Modal show={showModal} />
-      </div>
-    </App>
+    <div className="container">
+      <Header>
+        <Logo />
+        <Button
+          onClick={() => setShowModal(!showModal)}
+          type="primary"
+          styles="margin-right: 10px; margin-left: 30px;"
+        >
+          Добавить товар
+        </Button>
+        <Button type="primary">Добавить категорию</Button>
+      </Header>
+      <DataGrid />
+      <Modal show={showModal} />
+    </div>
   );
 };
