009d83eba909b4e064a6c52e8841be1c08bcf689
Ildar Nogmanov <nogmanov@gmail.com>
2020-01-23 12:45:35 +0300

    Add close modal icon

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import styled from "styled-components";
+import closeIcon from "./close.svg";
 
 const Modal = styled.div`
   width: 400px;
-  height: 400px;
+  height: 300px;
   background: white;
+  border: 1px solid orange;
   border-radius: 5px;
   box-shadow: -2rem 2rem 2rem rgba(0, 0, 0, 0.2);
   position: fixed;
@@ -21,8 +23,21 @@ const Modal = styled.div`
   text-align: center;
 `;
 
+const CloseIcon = styled.img`
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  width: 24px;
+  margin-right: 5px;
+`;
+
 // onclick outside feature
 
 export default ({ show, children }) => {
-  return <Modal show={show}>{children}</Modal>;
+  return (
+    <Modal show={show}>
+      <CloseIcon src={closeIcon} />
+      {children}
+    </Modal>
+  );
 };
