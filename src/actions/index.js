a193ff40a60a3de1a967bb2577c0e5f1aa649752
Ildar Nogmanov <nogmanov@gmail.com>
2020-01-30 08:29:12 +0300

    Add redux actions

---
+++ src/actions/index.js
@@ -0,0 +1,24 @@
+export const addProduct = product => ({
+  type: "ADD_PRODUCT",
+  product
+});
+
+export const editProduct = product => ({
+  type: "EDIT_PRODUCT",
+  product
+});
+
+export const removeProduct = id => ({
+  type: "REMOVE_PRODUCT",
+  id
+});
+
+export const addCategory = category => ({
+  type: "ADD_CATEGORY",
+  category
+});
+
+export const removeCategory = category => ({
+  type: "REMOVE_CATEGORY",
+  category
+});
