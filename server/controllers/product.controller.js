1006db339f410b7a05dbbf4a3d75df5851efd34b
Ildar Nogmanov <nogmanov@gmail.com>
2020-02-04 17:15:31 +0300

    Handle post request

--- server/controllers/product.controller.js
+++ server/controllers/product.controller.js
@@ -1,5 +1,17 @@
 import Product from "../models/product.model";
 
-export default (req, res) => {
-  res.send("Greetings from the Test controller!");
+export const create = (req, res, next) => {
+  const product = new Product({
+    name: req.body.name,
+    category: req.body.category,
+    price: req.body.price,
+    sell: req.body.sell
+  });
+
+  product.save(err => {
+    if (err) {
+      return next(err);
+    }
+    res.send("Product Created successfully");
+  });
 };
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,8 @@
 import { Router } from "express";
-import test from "../controllers/product.controller";
+import { create } from "../controllers/product.controller";
 
 const router = Router();
 
-router.get("/test", test);
+router.post("/", create);
 
 export default router;
