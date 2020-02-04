17269cfb7051bfdf6aa4199c253fefcbcb86350e
Ildar Nogmanov <nogmanov@gmail.com>
2020-02-04 16:28:59 +0300

    Add controller and routes for product

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import db from "./db";
 import cors from "cors";
 import "dotenv/config";
+import product from "./routes";
 import mongoose from "mongoose";
 
 // Set up mongoose connection
@@ -20,6 +21,8 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/products", product);
 
 app.get("/api/products", (req, res) => {
   res.status(200).send({
---
+++ server/controllers/product.controller.js
@@ -0,0 +1,5 @@
+import Product from "../models/product.model";
+
+export default (req, res) => {
+  res.send("Greetings from the Test controller!");
+};
