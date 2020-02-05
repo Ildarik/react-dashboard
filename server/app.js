514a6336d86118d9ca76e0be18b6df5ae8b2085e
Ildar Nogmanov <nogmanov@gmail.com>
2020-02-05 14:49:09 +0300

    Use singular in route

--- server/app.js
+++ server/app.js
@@ -26,7 +26,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/products", product);
+app.use("/product", product);
 app.use("/category", category);
 
 app.get("/api/products", (req, res) => {
