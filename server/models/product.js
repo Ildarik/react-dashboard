0356dffd8b506f742a3d1a34065a65744de0488c
Ildar Nogmanov <nogmanov@gmail.com>
2020-02-06 08:39:38 +0300

    Link products and categories

--- server/models/product.js
+++ server/models/product.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 
 let ProductSchema = new Schema({
   name: { type: String, required: true, max: 50 },
-  category: { type: String, required: true, max: 20 }, // TODO replace with { type: "ObjectId", ref: "Category" }
+  category: { type: "ObjectId", ref: "Category" },
   price: { type: Number, required: true },
   sell: { type: Number, required: true }
 });
