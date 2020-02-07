e010781a198db64a4c6c85b502cca96fcc97e4bb
Ildar Nogmanov <nogmanov@gmail.com>
2020-02-07 23:11:16 +0300

    Add ui for registration

--- client/src/components/Register/index.js
+++ client/src/components/Register/index.js
@@ -1,3 +1,55 @@
 import React from "react";
+import { Input, InputWrapper } from "../Modals/styled";
+import styled from "styled-components";
+import Button from "../Button";
 
-export default () => <div>Register</div>;
+const Centered = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+const Wrapper = styled(Centered)`
+  height: 90vh;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export default () => (
+  <div className="container">
+    <Wrapper>
+      <h1>Registration</h1>
+      <form onSubmit={null}>
+        <InputWrapper>
+          <Input
+            name="username"
+            value={null || ""}
+            onChange={null}
+            type="text"
+            placeholder="usern@me"
+          />
+        </InputWrapper>
+        <InputWrapper>
+          <Input
+            name="password"
+            value={null || ""}
+            onChange={null}
+            type="text"
+            placeholder="password"
+          />
+        </InputWrapper>
+        <InputWrapper>
+          <Input
+            name="confirm"
+            value={null || ""}
+            onChange={null}
+            type="text"
+            placeholder="confirm"
+          />
+        </InputWrapper>
+        <Centered>
+          <Button color="primary">Submit</Button>
+        </Centered>
+      </form>
+    </Wrapper>
+  </div>
+);
