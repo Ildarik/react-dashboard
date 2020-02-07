f4e79bc5b7245d934c300c03960cffc5f10888a2
Ildar Nogmanov <nogmanov@gmail.com>
2020-02-07 16:14:02 +0300

    Stub for public page

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,16 @@
 import React from "react";
+import styled from "styled-components";
+
+const Stub = styled.div`
+  line-height: 90vh;
+  text-align: center;
+  font-size: 24px;
+`;
 
 export default () => {
-  return <div>Hello</div>;
+  return (
+    <div className="container">
+      <Stub>Here will be a public page!</Stub>
+    </div>
+  );
 };
