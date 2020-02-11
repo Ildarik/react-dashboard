d22d61d5eb9145496337567de74f592da3c71131
Ildar Nogmanov <nogmanov@gmail.com>
2020-02-11 18:47:38 +0300

    Check auth in routes (later)

--- server/app.js
+++ server/app.js
@@ -30,26 +30,10 @@ app.use(express.json());
 app.use(
   session({
     secret: process.env.SECRET,
-    resave: false,
-    saveUninitialized: false
+    resave: true,
+    saveUninitialized: true
   })
 );
-
-const checkAuth = (req, res, next) => {
-  console.log(`req.session.username: ${req.session.username}`);
-  console.log(`req.session: ${JSON.stringify(req.session)}`);
-  if (
-    req.url === "/api/categories" &&
-    (!req.session || !req.session.username)
-  ) {
-    res.status(403).send("unauthorised");
-    return;
-  }
-
-  next();
-};
-
-app.use(checkAuth);
 
 app.use("/api/products", products);
 app.use("/api/categories", categories);
