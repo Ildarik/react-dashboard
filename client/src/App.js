7f94613c632682fb7ad0fa2722d561466c1ec2f0
Ildar Nogmanov <nogmanov@gmail.com>
2020-02-13 18:48:54 +0300

    Add ui for filters

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
-import { Row, Col, Card } from "antd";
+import { Row, Col, Card, Checkbox } from "antd";
 import { getProducts, getCategories } from "./actions";
 
 const Auth = styled.div`
@@ -17,7 +17,7 @@ const Auth = styled.div`
 // TODO think about where is a better place to get products
 
 export default () => {
-  const { products } = useSelector(state => state);
+  const { products, categories } = useSelector(state => state);
 
   const dispatch = useDispatch();
 
@@ -29,16 +29,23 @@ export default () => {
     fetchAPI();
   }, [dispatch]);
 
+  const filters = categories.map(category => category.name);
   return (
     <div className="container">
       <Auth>
         <Link to="/login">Login</Link>
         <Link to="/register">Register</Link>
       </Auth>
-      <Row gutter={[16, 16]}>
+      <Row gutter={[16, 16]} justify="center">
+        <Col>
+          <Checkbox.Group options={filters} defaultValue={["Man"]} />
+        </Col>
+      </Row>
+      <Row gutter={[16, 16]} justify="center">
+        {/* TODO add sceleton for cards */}
         {products.map(product => (
           <Col xs={24} sm={12} md={8} lg={6} key={product._id}>
-            <Card hoverable style={{ width: 240 }}>
+            <Card hoverable style={{ width: 200 }}>
               <Card.Meta
                 title={product.name}
                 description={`${product.sell}$`}
